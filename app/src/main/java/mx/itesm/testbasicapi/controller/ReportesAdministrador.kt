88c5dbca3203d600782d9b3a2d3571ce950357ea
Cesar Gomez <a00828456@itesm.mx>
2021-10-11 22:56:24 -0500

    Cambios menores ids fragments

--- app/src/main/java/mx/itesm/testbasicapi/controller/ReportesAdministrador.kt
+++ app/src/main/java/mx/itesm/testbasicapi/controller/ReportesAdministrador.kt
@@ -14,6 +14,6 @@ class ReportesAdministrador : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reportes_administrador, container, false)
+        return inflater.inflate(R.layout.fragment_reporte_individual_administrador, container, false)
     }
 }
